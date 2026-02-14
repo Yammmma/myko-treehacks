90523cd67b56bb8794258112ee520061f6ea6082
Yuma Soerianto <hi@madebyyuma.com>
2026-02-13 21:38:47 -0800

    Initialise project

---
+++ myko-treehacks/myko_treehacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  myko_treehacksApp.swift
+//  myko-treehacks
+//
+//  Created by Yuma Soerianto on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct myko_treehacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
