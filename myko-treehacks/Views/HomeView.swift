969bf4fc4e90869cf40df662a98195060974893f
Amy <amy.q.key@gmail.com>
2026-02-13 22:34:28 -0800

    test commit

---
+++ myko-treehacks/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  myko-treehacks
+//
+//  Created by Amy Sun Key on 2/13/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
